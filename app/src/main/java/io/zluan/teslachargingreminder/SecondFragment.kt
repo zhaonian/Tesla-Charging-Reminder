578e6f6691b1cfbce295aa92aefa3c366080f8f0
Zhaonian Luan <zluan@google.com>
2021-01-13 23:10:53 -0800

    reset project structure

--- app/src/main/java/io/zluan/teslachargingreminder/SecondFragment.kt
+++ app/src/main/java/io/zluan/teslachargingreminder/SecondFragment.kt
@@ -1,38 +1,38 @@
-package io.zluan.teslachargingreminder.ui.home
+package io.zluan.teslachargingreminder
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
-
-import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 
-import io.zluan.teslachargingreminder.R
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class SecondFragment : Fragment() {
 
-class HomeSecondFragment : Fragment() {
-
-    private val args: HomeSecondFragmentArgs by navArgs()
+    private val args: SecondFragmentArgs by navArgs()
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_second, container, false)
+        return inflater.inflate(R.layout.fragment_second, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<TextView>(R.id.textview_home_second).text =
-                getString(R.string.hello_home_second, args.myArg)
+        view.findViewById<TextView>(R.id.textview_second).text =
+                getString(R.string.hello_second_fragment, args.myArg)
 
-        view.findViewById<Button>(R.id.button_home_second).setOnClickListener {
-            findNavController().navigate(R.id.action_HomeSecondFragment_to_HomeFragment)
+        view.findViewById<Button>(R.id.button_second).setOnClickListener {
+            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
     }
 }
